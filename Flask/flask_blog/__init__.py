c6bdb78b5b6de756f611f4c3cf6f3e29229c6106
Gautam3994 <gautam3994@gmail.com>
2020-01-20 23:56:54 +0530

    Beast

--- Flask/flask_blog/__init__.py
+++ Flask/flask_blog/__init__.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'cf8684c33f10290611a8a2616c40efcc'  # Got using secrets.token_hex(16) from secrets module
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
 # after this import db in cmc
 # then db.create_all() and then import User and Posts table and then use query
 
--- Flask/flask_blog/forms.py
+++ Flask/flask_blog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo, Length
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from flask_blog.models import User, Posts
 
 
 class RegistrationForm(FlaskForm):
@@ -9,6 +10,16 @@ class RegistrationForm(FlaskForm):
     password = PasswordField(label="Password", validators=[DataRequired()])
     confirm_password = PasswordField(label="Confirm Passowrd", validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField(label='Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError("user name already exists")
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError("email already exists")
 
 
 class LoginForm(FlaskForm):
