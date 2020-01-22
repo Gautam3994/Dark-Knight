732f8b2bd62b728eb1a56991e68ed8376da25c4d
Gautam3994 <gautam3994@gmail.com>
2020-01-22 23:07:39 +0530

    TODO pagination

--- Flask/flask_blog/routes.py
+++ Flask/flask_blog/routes.py
@@ -103,14 +103,15 @@ def newpost():
             file_name = hex_name + file_ext
             file_path = os.path.join(app.root_path, f'static/files/{str(current_user.id)}', file_name)
             form.file.data.save(file_path)
-            # current_user.posts.file = file_name
-        if file_name:
+            current_user.posts.file = file_name
             post = Posts(title=form.title.data, content=form.content.data, author=current_user, file=file_name)
+        else:
+            post = Posts(title=form.title.data, content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post has been added successfully', 'success')
         return redirect(url_for('home'))
-    return render_template("newpost.html", title="New Post", form=form)
+    return render_template("newpost.html", title="New Post", form=form, legend='Create Post')
 
 
 @app.route("/post/<int:post_id>")
@@ -119,11 +120,41 @@ def post(post_id):
     return render_template('post.html', title=post.title, post=post)
 
 
-@app.route("/post/<int:post_id>/update")
+@app.route("/post/<int:post_id>/update",  methods=['GET', 'POST'])
 @login_required
 def update_post(post_id):
     post = Posts.query.get_or_404(post_id)
     if post.author != current_user:
         abort(403)
     form = PostForm()
-    return render_template("newpost.html", title="Update Post", form=form)
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        if form.file.data:
+            hex_name = secrets.token_hex(8)
+            _, file_ext = os.path.splitext(form.file.data.filename)
+            file_name = hex_name + file_ext
+            file_path = os.path.join(app.root_path, f'static/files/{str(current_user.id)}', file_name)
+            form.file.data.save(file_path)
+            post.file = file_name
+            # current_user.post.file = form.file.data.filename
+        db.session.commit()
+        flash("Your post was updated successfully", "success")
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+        form.file.data = post.file
+    return render_template("newpost.html", title="Update Post", form=form, legend='Update Post')
+
+
+@app.route("/post/<int:post_id>/delete",  methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Posts.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted', 'success')
+    return redirect(url_for('home'))
