0ec4cda96ebb2734460354565bc45d1da40b8f4c
Gautam3994 <gautam3994@gmail.com>
2020-02-01 20:20:03 +0530

    next li items

--- Flask/flask_blog/config.py
+++ Flask/flask_blog/config.py
@@ -3,7 +3,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')  # Got using secrets.token_hex(16) from secrets module
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///site.db"
+        # os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
